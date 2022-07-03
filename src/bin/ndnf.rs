40ab2d3b2c001b61e53aea8dd8c8aae0d220f6f2
Simon de Vlieger <cmdr@supakeen.com>
2022-07-04 00:43:29 +0200

    setup: some more subcommand layout

--- src/bin/ndnf.rs
+++ src/bin/ndnf.rs
@@ -1,12 +1,18 @@
-use clap;
-
 fn make_cli() -> clap::Command<'static> {
     clap::command!()
         .propagate_version(true)
         .arg(clap::arg!(-j --json "Use JSON input and output").required(false))
         .subcommand_required(true)
         .subcommand(make_cli_install())
+        .subcommand(make_cli_upgrade())
         .subcommand(make_cli_remove())
+        .subcommand(make_cli_reinstall())
+}
+
+fn make_cli_upgrade() -> clap::Command<'static> {
+    clap::command!("upgrade")
+        .about("Upgrade a package or packages on your system")
+        .arg(clap::arg!(<package> "Package(s) to upgrade").multiple(true))
 }
 
 fn make_cli_install() -> clap::Command<'static> {
@@ -14,16 +20,26 @@ fn make_cli_install() -> clap::Command<'static> {
         .about("Install a package or packages on your system")
         .arg(clap::arg!(-q --quiet "Quiet operation (less output)").required(false))
         .arg(clap::arg!(-v --verbose "Verbose operation (more output)").required(false))
-        .arg(clap::arg!(-y --assumeyes "Automatically answer yes for all questions").required(false))
+        .arg(
+            clap::arg!(-y --assumeyes "Automatically answer yes for all questions").required(false),
+        )
         .arg(clap::arg!(-n --assumeno "Automatically answer no for all questions").required(false))
         .arg(clap::arg!(--nodocs "Do not install package documentation").required(false))
-        .arg(clap::arg!(-r --refresh "Set metadata as expired before running the command").required(false))
+        .arg(
+            clap::arg!(-r --refresh "Set metadata as expired before running the command")
+                .required(false),
+        )
         .arg(clap::arg!(<package> "Package(s) to install").multiple(true))
 }
 
+fn make_cli_reinstall() -> clap::Command<'static> {
+    clap::command!("reinstall")
+        .about("Reinstall a package or packages on your system")
+        .arg(clap::arg!(<package> "Package(s) to reinstall").multiple(true))
+}
+
 fn make_cli_remove() -> clap::Command<'static> {
-    clap::command!("remove")
-        .about("Remove a package or packages from your system")
+    clap::command!("remove").about("Remove a package or packages from your system")
 }
 
 fn main() {
--- src/bin/ndnfd.rs
+++ src/bin/ndnfd.rs
@@ -1,5 +1,3 @@
-use clap;
-
 fn main() {
     println!("Hello, world!");
 }
