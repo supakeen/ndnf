7c695945be017cb41f344a7accfe54e51d06e7b8
Simon de Vlieger <cmdr@supakeen.com>
2022-07-04 12:38:28 +0200

    install: options for subcommands
    These options should really move onto the main command though?

--- src/bin/ndnf.rs
+++ src/bin/ndnf.rs
@@ -12,6 +12,26 @@ fn make_cli() -> clap::Command<'static> {
 fn make_cli_upgrade() -> clap::Command<'static> {
     clap::command!("upgrade")
         .about("Upgrade a package or packages on your system")
+        .arg(
+            clap::arg!(-q --quiet "Quiet operation (less output)")
+                .required(false)
+                .conflicts_with("verbose"),
+        )
+        .arg(
+            clap::arg!(-v --verbose "Verbose operation (more output)")
+                .required(false)
+                .conflicts_with("quiet"),
+        )
+        .arg(
+            clap::arg!(-y --assumeyes "Automatically answer yes for all questions")
+                .required(false)
+                .conflicts_with("assumeno"),
+        )
+        .arg(
+            clap::arg!(-n --assumeno "Automatically answer no for all questions")
+                .required(false)
+                .conflicts_with("assumeyes"),
+        )
         .arg(clap::arg!(<package> "Package(s) to upgrade").multiple(true))
 }
 
@@ -49,17 +69,57 @@ fn make_cli_install() -> clap::Command<'static> {
 fn make_cli_reinstall() -> clap::Command<'static> {
     clap::command!("reinstall")
         .about("Reinstall a package or packages on your system")
+        .arg(
+            clap::arg!(-q --quiet "Quiet operation (less output)")
+                .required(false)
+                .conflicts_with("verbose"),
+        )
+        .arg(
+            clap::arg!(-v --verbose "Verbose operation (more output)")
+                .required(false)
+                .conflicts_with("quiet"),
+        )
+        .arg(
+            clap::arg!(-y --assumeyes "Automatically answer yes for all questions")
+                .required(false)
+                .conflicts_with("assumeno"),
+        )
+        .arg(
+            clap::arg!(-n --assumeno "Automatically answer no for all questions")
+                .required(false)
+                .conflicts_with("assumeyes"),
+        )
         .arg(clap::arg!(<package> "Package(s) to reinstall").multiple(true))
 }
 
 fn make_cli_remove() -> clap::Command<'static> {
     clap::command!("remove")
         .about("Remove a package or packages from your system")
+        .arg(
+            clap::arg!(-q --quiet "Quiet operation (less output)")
+                .required(false)
+                .conflicts_with("verbose"),
+        )
+        .arg(
+            clap::arg!(-v --verbose "Verbose operation (more output)")
+                .required(false)
+                .conflicts_with("quiet"),
+        )
+        .arg(
+            clap::arg!(-y --assumeyes "Automatically answer yes for all questions")
+                .required(false)
+                .conflicts_with("assumeno"),
+        )
+        .arg(
+            clap::arg!(-n --assumeno "Automatically answer no for all questions")
+                .required(false)
+                .conflicts_with("assumeyes"),
+        )
         .arg(clap::arg!(<package> "Package(s) to install").multiple(true))
 }
 
 fn main() {
-    let matches = make_cli().get_matches();
+    let _matches = make_cli().get_matches();
 
     println!("Hello, world!");
 }
